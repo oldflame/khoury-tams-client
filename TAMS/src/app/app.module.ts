208f1cdd9435b745fff2610082142643b413f61e
Dhaval Dedhia <dhavl32@gmail.com>
2020-03-16 20:09:54 -0400

    Created directory structure for login, register and auth

--- TAMS/src/app/app.module.ts
+++ TAMS/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
