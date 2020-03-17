9470fcd93ce16902badf0c5f988d4b346676a06e
Dhaval Dedhia <dhavl32@gmail.com>
2020-03-17 15:29:32 -0400

    Implemented register frontend

--- TAMS/src/app/modules/auth/register/register.component.ts
+++ TAMS/src/app/modules/auth/register/register.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8)]),
+    firstName: new FormControl("", [Validators.required]),
+    lastName: new FormControl("", [Validators.required]),
+    phoneNumber: new FormControl("", [Validators.required, Validators.maxLength(10)]),
+    role: new FormControl("", [Validators.required])
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
