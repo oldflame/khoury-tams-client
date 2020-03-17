0770d2a0f2d5e0e20005267ecce0dcd8c993e706
Dhaval Dedhia <dhavl32@gmail.com>
2020-03-17 16:10:05 -0400

    Added Routing for Homepage

--- TAMS/src/app/app.routing.ts
+++ TAMS/src/app/app.routing.ts
@@ -1,12 +1,24 @@
 import { AuthLayoutComponent } from "./layout/auth-layout/auth-layout.component";
 import { Routes } from "@angular/router";
 import { AuthModule } from './modules/auth/auth.module';
+import { HomeModule } from './modules/home/home.module';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
 export const AppRoutes: Routes = [
   {
     path: "",
     redirectTo: "login",
     pathMatch: "full"
+  },
+  {
+    path: "account",
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: "home",
+        loadChildren: () => HomeModule
+      }
+    ]
   },
   {
     path: "",
