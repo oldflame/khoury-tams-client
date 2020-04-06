762478c445d770fdc790991db0ad0747a1b340a7
Dhaval Dedhia <dhavl32@gmail.com>
2020-04-06 18:52:21 -0400

    Added login route on successful register

--- TAMS/src/app/modules/auth/register/register.component.ts
+++ TAMS/src/app/modules/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { UserService } from 'src/app/services/user.service';
+import { UserService } from "src/app/services/user.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "register",
@@ -23,11 +24,17 @@ export class RegisterComponent implements OnInit {
     role: new FormControl("", [Validators.required])
   });
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {}
 
   onRegister() {
-    this.userService.registerUser(this.registerForm.value).subscribe();
+    this.userService
+      .registerUser(this.registerForm.value)
+      .subscribe((response: boolean) => {
+        if (response) {
+          this.router.navigate(["/login"]);
+        }
+      });
   }
 }
