66191b57f2029b88870a1a6289fe55b2cf147c01
Dhaval Dedhia <dhavl32@gmail.com>
2020-04-07 00:58:55 -0400

    Get auth token from server and save it to local storage

--- TAMS/src/app/services/user.service.ts
+++ TAMS/src/app/services/user.service.ts
@@ -7,13 +7,17 @@ import {
 import { DataService } from "./data.service";
 import { User } from "../models/user";
 import { map, catchError } from "rxjs/operators";
-import { of } from "rxjs";
+import { of, BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserService {
   constructor(private http: HttpClient, private dataService: DataService) {}
+
+  private userSubject = new BehaviorSubject(null);
+  user$: Observable<User> = this.userSubject.asObservable();
+
   registerUser(user: User) {
     return this.dataService.sendPOST(`/register`, user).pipe(
       map((res: HttpResponse<any>) => {
@@ -34,6 +38,9 @@ export class UserService {
     return this.dataService.sendPOST(`/login`, user).pipe(
       map((res: HttpResponse<any>) => {
         if (res.status === 200) {
+          this.userSubject.next(res.body);
+          localStorage.setItem("user", JSON.stringify(res.body));
+          localStorage.setItem("token", res.body.authToken);
           return true;
         } else {
           return false;
