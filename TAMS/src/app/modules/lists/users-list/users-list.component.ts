3386b209c7cdcf9f300b6275ee073cb4896dccb6
Dhaval Dedhia <dhavl32@gmail.com>
2020-04-21 03:35:38 -0400

    Bug fix for follow unfollow button

--- TAMS/src/app/modules/lists/users-list/users-list.component.ts
+++ TAMS/src/app/modules/lists/users-list/users-list.component.ts
@@ -15,26 +15,30 @@ import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
 import { FormControl } from "@angular/forms";
 import { User } from "src/app/models/user";
-import { UserService } from 'src/app/services/user.service';
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: "users-list",
   templateUrl: "./users-list.component.html",
   styleUrls: ["./users-list.component.css"],
 })
-export class UsersListComponent implements OnInit {
+export class UsersListComponent implements OnInit, OnChanges {
   @Input("usersList") usersList: User[];
   @Output("followUser") followUser = new EventEmitter();
   @Output("unFollowUser") unFollowUser = new EventEmitter();
 
   currentUser: User;
 
-  constructor(private userService: UserService) {
-  }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.currentUser = JSON.parse(this.userService.getUserData());
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.usersList && changes.usersList.currentValue != null) {
+      this.currentUser = JSON.parse(this.userService.getUserData());
+    }
   }
   followUserClicked(user: User) {
     this.followUser.emit({ user });
